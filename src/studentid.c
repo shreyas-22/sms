00de336c80312342d10fe0cee16b2db011c4081a
Shreyas <shreyas002.sk@gmail.com>
2020-09-21 10:01:40 +0530

    Implemented delete,update and print list function

--- src/studentid.c
+++ src/studentid.c
@@ -81,7 +81,7 @@ int create_entry(student_t **head)
     return 0;
 }
   
-int print_entry(student_t *head, int adm_no)
+int search_print(student_t *head, int adm_no)
 {
     student_t *ptr = head;
     while(ptr != NULL)
@@ -110,6 +110,103 @@ int print_entry(student_t *head, int adm_no)
     return 0;
 }
 
+int Delete_entry(student_t **head)
+{
+    int adm_no;
+    student_t *ptr = *head;
+    student_t *prev = NULL;
+    printf("Enter the admission number to be Deleted\n");
+    scanf("%d",&adm_no);
+    
+    while(ptr != NULL)
+    {   /* match is found */
+        if(ptr->adm_no == adm_no)
+        {   /* match found at first node */
+            if(prev == NULL)
+            {
+                *head = ptr->next;
+                free(ptr);
+                ptr = NULL;
+                break;
+            }   /* match found at other node */
+            else
+            {
+                prev->next = ptr->next;
+                free(ptr);
+                ptr = NULL;
+                break;
+            }
+        }
+        prev = ptr;
+        ptr = ptr->next;
+    }
+}
+
+int print_entire_list(student_t **head)
+{
+    printf("HERE\n");
+    student_t *ptr = *head;
+    while(ptr != NULL)
+    {
+        printf("----------------------------------\n");
+        printf("The student details are:\n");
+        printf("----------------------------------\n");   
+        printf("Name: %s\n",ptr->name);
+        printf("DOB (dd-mm-yyyy): %hu-%hu-%hu\n",ptr->dob.day, 
+        ptr->dob.month, ptr->dob.year);
+        printf("Phone number: %lu\n",ptr->phone_no);
+        printf("Branch: %s\n",br_num_to_name(ptr->branch));
+        printf("usn: %s\n",ptr->usn);
+        printf("Admission number: %u\n",ptr->adm_no);
+        ptr = ptr->next;
+    }
+}
+
+int update_entry(student_t **head, int adm_no)
+{
+    int result;
+    student_t *ptr = *head;
+    while(ptr != NULL)
+    {
+        if(ptr->adm_no == adm_no)
+        {
+            printf("##################################\n");
+            printf("Update the details of the student:\n");
+            printf("##################################\n");
+            printf("Enter name: ");
+            scanf("%s",ptr->name);
+            printf("Enter DOB (dd-mm-yyyy): ");
+            scanf("%hu-%hu-%hu", &ptr->dob.day, 
+            &ptr->dob.month, &ptr->dob.year);
+            printf("Enter phone number: ");
+            scanf("%lu", &ptr->phone_no);
+            do
+            {
+            printf("Enter usn: ");
+            scanf("%s",ptr->usn);
+            result = usn_to_branch(ptr->usn);
+            if(result == (-1))
+            {
+            printf("wrong usn!!\n");
+            }
+            } while(result == (-1));
+            ptr->branch = result;
+        }
+        else
+        {
+            printf("details not found\n");
+        }
+        ptr = ptr->next;
+    }
+}
+
+
+    
+        
+            
+        
+    
+
         
 
 int main()
@@ -117,15 +214,23 @@ int main()
     int result = 0;
     int adm_no = 0;   
     create_entry(&list);
-     
-    do
+    
+    print_entire_list(&list);
+    /*do
     {
-        printf("Enter adm.no: ");
+        printf("Enter adm.no to be searched: ");
         scanf("%d",&adm_no);
-        result = print_entry(list, adm_no);
+        result = search_print(list, adm_no);
         if(result == (-1))
         {
             printf("Details not found\n");
         }
     } while(result == (-1));  
+    Delete_entry(&list); */
+    printf("Enter adm.no to be searched: ");
+        scanf("%d",&adm_no);
+    update_entry(&list , adm_no);
+    print_entire_list(&list);
+    
+   
 }
